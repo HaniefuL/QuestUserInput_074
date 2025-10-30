438e360d7b2897c51e17dc57995987225963ea01
HaniefuL <haniefulgeofarry@gmail.com>
2025-10-30 13:08:56 +0700

    menambahkan padding horizontal 5dp dan vertikal 15dp pada komponen Column

--- app/src/main/java/com/example/questuserinput_074/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_074/TeksEdit.kt
@@ -117,6 +117,9 @@ fun FormDataDiri(modifier: Modifier
                 .height(height = 100.dp)
                 .width(width = 300.dp)
         ) {
+            Column(
+                modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp)
+            )
         }
     }
 }
