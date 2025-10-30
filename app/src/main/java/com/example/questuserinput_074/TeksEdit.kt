710050412c98465b3be83d82eb19c659d2e078f6
HaniefuL <haniefulgeofarry@gmail.com>
2025-10-30 12:37:20 +0700

    menambahkan Button

--- app/src/main/java/com/example/questuserinput_074/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_074/TeksEdit.kt
@@ -1,13 +1,28 @@
 package com.example.questuserinput_074
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 
-@composable
+annotation class composable
+
+@Composable
 fun FormDataDiri(modifier: Modifier
 ){
     var textNama by remember { mutableStateOf("") }
@@ -31,11 +46,13 @@ fun FormDataDiri(modifier: Modifier
             modifier = Modifier.width(250.dp),
             label = { Text(text = " Nama Lenkkap") },
             onValueChange = {
+                val it = null
                 textNama = it
             }
         )
         Row {
             gender.forEach { item ->
+                val textJk = null
                 Row (modifier = Modifier.selectable(
                     selected = textJk == item,
                     onClick = { textJk = item}
@@ -55,9 +72,18 @@ fun FormDataDiri(modifier: Modifier
             modifier = Modifier.width(250.dp),
             label = {Text(text = "Alamat Lengkap")},
             onValueChange = {
+                val it = null
                 textAlamat = it
             }
-            )
-        }
+        )
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource)
+            id = 30dp
+            )),
+            thickness = dimensionResource(1dp),
+            color = Color.DarkGray
+        )
+        Button(
+        )
     }
 }
