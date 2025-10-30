016283f1d17d8dc73fc5c8ad9a99cd94dfb2ec56
HaniefuL <haniefulgeofarry@gmail.com>
2025-10-30 13:07:34 +0700

    menambahkan ElevatedCard berwarna hitam dengan elevasi 10dp dan ukuran 300x100dp

--- app/src/main/java/com/example/questuserinput_074/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_074/TeksEdit.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -106,5 +109,14 @@ fun FormDataDiri(modifier: Modifier
                 thickness = 1.dp,
                 color = Color.DarkGray
             )
+
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(height = 100.dp)
+                .width(width = 300.dp)
+        ) {
+        }
     }
 }
